8c04a0d54c5d22f110730a272546f397d8cd21c6
Prateek3909 <65811404+Prateek3909@users.noreply.github.com>
2024-05-13 16:39:59 +0000

    dataType Conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score=undefined;
+console.log(typeof(score));
+let valueInNumber=Number(score);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+//"33"-> 33
+// "33abc"-> NaN
+//true-> 1, false-> 0
+let loggedIn=1;
+let booleanIsLoggedIn=Boolean(loggedIn);
+console.log(booleanIsLoggedIn);
+
+//1->true, 2->false, 
+// ""-> false, "Prateek-> true"
+
+let someNumber=29;
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+console.log(typeof(stringNumber));
