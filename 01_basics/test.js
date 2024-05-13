2e566f68a89cbfdb4c664fdfafe2781adef2fa7c
Prateek3909 <65811404+Prateek3909@users.noreply.github.com>
2024-05-13 13:51:48 +0000

    changes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,16 @@
 console.log("Prateek");
+var studentId="213312";
+const studentRollNo="32535";
+let studentMobNo="9213213132";
+
+// assignment to constant variable is not allowed.
+// studentRollNo="65765";
+
+// redeclare of variable which having let keyword is not allowed, but assignment is allowed.
+studentMobNo="54654654646";
+//redeclare of variable which is having var keyword is allowed and also assignment is allowed.
+var studentId="547574";
+
+console.log(studentId);
+console.log(studentRollNo);
+console.log(studentMobNo);
