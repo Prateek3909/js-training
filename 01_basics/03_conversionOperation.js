05c71533651fe155c720f7b8d25e605034a13e55
Prateek3909 <65811404+Prateek3909@users.noreply.github.com>
2024-05-14 17:39:21 +0000

    changes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -18,3 +18,14 @@ let someNumber=29;
 let stringNumber=String(someNumber);
 console.log(stringNumber);
 console.log(typeof(stringNumber));
+
+let posValue=2;
+let negValue=-posValue;
+console.log(negValue);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2); //JavaScript processes them from left to right. So, "1"+2 results in "12", and then "12"+2 results in "122", not 14.
+console.log(1+2+"2");
+
+console.log(Boolean("Prateek"));
