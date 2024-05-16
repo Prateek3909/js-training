97c4e23f7c97e4474907fea58194a6e1d7171da4
Prateek3909 <65811404+Prateek3909@users.noreply.github.com>
2024-05-16 17:44:42 +0000

    data type summary

---
+++ 01_basics/datatypesSummary.js
@@ -0,0 +1,30 @@
+// Primitive Data Types ------------------------------
+// Numbers
+// String
+// Boolean
+// Symbol
+// null
+// undefined
+// BigInt
+
+//Non-Primitive Data Types (Reference Data Types) ------------------------
+//  Array
+//  Objects
+//  Functions
+//  Collections
+//  Dates
+
+let bigNum = 123422222222222222222222222222222222222n
+console.log(bigNum);
+
+let myData={
+    name:"Prateek",
+    age:25
+}
+let updatedData=myData;
+
+myData.name="Kumar";
+
+console.log(myData.age);
+console.log(myData.name);
+console.log(updatedData);
